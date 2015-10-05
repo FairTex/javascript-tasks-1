5808a6fc3f8f1bd4e3ecb570106a24e6518a7cb9
FairTex <makarov906@gmail.com>
2015-10-06 00:03:00 +0500

    Макаров Виталий

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,7 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
+var romanTime = '';
+var magicDigits = [50, 40, 10, 9, 5, 4, 1];
+var dict = { 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I' };
 
 // Немного замечательного кода и магии
